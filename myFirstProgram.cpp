2ff208f29e0959cc6c2a59228ab445490721195b
ShafiMunshi <shafimunshi111@gmail.com>
2024-01-04 22:12:27 +0600

    classs2

--- myFirstProgram.cpp
+++ myFirstProgram.cpp
@@ -1,42 +1,32 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header file
+using namespace std; // namespace declaration
 
 
-// This is a main function
-
-
-/*
-
-    helllo this is main function..
-    
-
-*/
-
-
-/*
-    fsjdflasj
-*/
+// this is a main function- where compiler start code excutingg
 int main(){
     int my_First_Number100 = 30; // 4 byte = 32 bit
 
-    // int mySecondNumber100 = 30; // 4 byte = 32 bit
+    int mySecondNumber100 = 30; // 4 byte = 32 bit
 
-    // cout<<"hello world"<<endl;
+    cout<<"hello world"<<endl;
 
-    // cout<< myFirstNumber100 <<endl;
-
-
-    // float f = 189.234232342; // 4 byte
-
-    // cout<< sizeof(float) << endl;
+    cout<< my_First_Number100 <<endl;// printing the value
 
 
-    // cout << f << endl;
+    float f = 189.234232342; // 4 byte
 
-    // int maxx=2147483647; // 10^9
 
-    // cout<<maxx<<endl;
+    // this function is used for get how much memory consume each data
+     cout<< sizeof(float) << endl;  
+     cout<< sizeof(int) << endl;  
 
-    int min = -2147483649;
+
+    cout << f << endl;
+
+    int maxx=2147483647; // 10^9 is the max limit
+
+    cout<<maxx<<endl;
+
+    int min = -2147483648; // it is the minimum limit
     cout<< min<< endl;
 }
